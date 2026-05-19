30a5caf07701afe583f5e5f12efaac2afcdd0f8b
Dhananjay <dhananjaymukhiya2005@gmail.com>
2026-05-19 13:25:32 +0530

    complete nineteenth project

---
+++ 19_PasswordStrength/script.js
@@ -0,0 +1,26 @@
+const password = document.querySelector("#password")
+const message = document.querySelector("#message")
+const strenght = document.querySelector("#strenght")
+
+password.addEventListener("input", () => {
+    if (password.value.length > 0) {
+        message.style.display = "block"
+    } else {
+        message.style.display = "none"
+        return
+    }
+
+    if (password.value.length < 4) {
+        strenght.innerHTML = "weak"
+        password.style.borderColor = "#ff5925"
+        message.style.color = "#ff5925"
+    } else if (password.value.length >= 4 && password.value.length < 8) {
+        strenght.innerHTML = "medium"
+        password.style.borderColor = "yellow"
+        message.style.color = "yellow"
+    } else if (password.value.length >= 8) {
+        strenght.innerHTML = "strong"
+        password.style.borderColor = "#26d730"
+        message.style.color = "#26d730"
+    }
+})
